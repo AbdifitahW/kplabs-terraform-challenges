75d7e8a09a2c31ddec821e738808b94ab86c72f7
AbdifitahW <abdifitah.warsame@gmail.com>
2024-02-28 21:19:05 -0500

    "intial challenge 3 "

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -1,9 +1,15 @@
-
 variable "instance_config" {
   type = map
+  // AMI Ids invalid for us-east-1 region 
   default = {
     instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
     instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
   }
 }
 
+resource "aws_instance" "instance" {
+ for_each = var.instance_config 
+
+ ami = each.value.ami
+ instance_type = each.value.ami
+}
